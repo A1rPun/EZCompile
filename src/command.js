49a3a3cf4458d132c3e109ec20486b827bb4229f
A1rPun <thombakker@gmail.com>
2021-11-09 23:00:40 +0100

    :scroll: Improve output

--- src/command.js
+++ src/command.js
@@ -6,17 +6,19 @@ async function cmd(command, ...args) {
 
   return new Promise((res, rej) => {
     proc.stdout.on('data', (data) => {
+      logger.info('stdout');
       console.log(data.toString());
     });
 
-    proc.stderr.on('data', (data) => {
-      rej(data.toString());
+    proc.stderr.on('data', async (data) => {
+      logger.error('stderr');
+      console.log(data.toString());
     });
 
     proc.on('error', (error) => {
       rej(error);
     });
-
+    
     proc.on('exit', (code) => {
       res(code);
     });
